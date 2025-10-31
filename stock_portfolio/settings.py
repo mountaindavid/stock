7ecba7512235d41a84b03dfdc12a9ab413485da9
David <16david93@gmail.com>
2025-10-30 19:57:51 -0700

    token expiration 1000000

--- stock_portfolio/settings.py
+++ stock_portfolio/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1000000),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
 }
